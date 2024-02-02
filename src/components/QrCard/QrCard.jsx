45d1f0a9d2f3a5732c81abd76c52ea9cfb823bfe
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-02-02 22:14:48 +0530

    content fix

--- src/components/QrCard/QrCard.jsx
+++ src/components/QrCard/QrCard.jsx
@@ -1,26 +1,7 @@
 import styles from "./QrCard.module.scss"
 const QrCard = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.card}>
-                <div className={styles.box}>
-                    <form>
-                        <label>URL:
-                            <input
-                                type="text"
-                                placeholder="Enter your url...."
-                            />
-                        </label>
-                        <label>Text:
-                            <input
-                                type="text"
-                                placeholder="Enter your text...."
-                            />
-                        </label>
-                    </form>
-                </div>
-            </div>
-        </div>
+        <div>QrCard</div>
     )
 }
 
